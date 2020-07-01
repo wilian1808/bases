9e9951b427aabbebe1c60b5d28ee88129dcff8df
wilian1808 <wilian.walli@gmail.com>
2020-07-01 13:54:15 -0500

    revisar

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 const NUMBER_BASES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 const CONVERTIR = document.getElementById('convertir');
 const TEXT_RESPONSE = (<HTMLInputElement>document.getElementById('numeroFinal'));
+const INPUT_NUMBER = (<HTMLInputElement>document.getElementById('numeroOriginal'));
+const ALERT_MESSAGE = (<HTMLInputElement>document.getElementById('alertMessage'));
 
 // se activa cuando querems convertir o hacemos click al boton de convertir
 CONVERTIR.addEventListener('submit', (e) => {
@@ -149,3 +151,49 @@ const DeleteZero = (number: string): string => {
 
     return newNumber.reverse().join('');
 }
+
+INPUT_NUMBER.addEventListener('keyup', e => {
+    let bo = (<HTMLInputElement>document.getElementById('baseOriginal')).value;
+    let number = INPUT_NUMBER.value;
+    let newNumber = number.split('.');
+    let parteEntera = newNumber[0];
+    let parteDecimal = newNumber[1];
+    let baseEntera = parseInt(bo);
+
+    // parte entera
+    for (let i = 0; i < parteEntera.length; i++) {
+        let messageError = 'El numero ingresado es incorrecto';
+        let clase = 'form_message--view';
+
+        switch (parteEntera[i]) {
+            case 'a' || 'A':
+                (10 >= baseEntera) ? ALERT_MESSAGE.classList.add(clase) : ALERT_MESSAGE.classList.remove(clase);
+                break;
+            case 'b' || 'B':
+                (11 >= baseEntera) ? ALERT_MESSAGE.classList.add(clase) : ALERT_MESSAGE.classList.remove(clase);
+                break;
+            case 'c' || 'C':
+                (12 >= baseEntera) ? ALERT_MESSAGE.classList.add(clase) : ALERT_MESSAGE.classList.remove(clase);
+                break;
+            case 'b' || 'D':
+                (13 >= baseEntera) ? ALERT_MESSAGE.classList.add(clase) : ALERT_MESSAGE.classList.remove(clase);
+                break;
+            case 'e' || 'E':
+                (14 >= baseEntera) ? ALERT_MESSAGE.classList.add(clase) : ALERT_MESSAGE.classList.remove(clase);
+                break;
+            case 'f' || 'F':
+                (15 >= baseEntera) ? ALERT_MESSAGE.classList.add(clase) : ALERT_MESSAGE.classList.remove(clase);
+                break;
+            default:
+                if (parseInt(parteEntera[i]) >= baseEntera) {
+                    ALERT_MESSAGE.classList.add(clase);
+                } else {
+                    ALERT_MESSAGE.classList.remove(clase);
+                }
+                break;
+        }
+    }
+
+    console.log(parteDecimal);
+
+})
